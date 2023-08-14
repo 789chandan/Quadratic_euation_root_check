d33158d9618718fefdcca5f45fc2e3ffeb8a5e81
CHANDAN KUMAR <111732919+789chandan@users.noreply.github.com>
2023-08-14 22:57:36 +0530

    Update Quadratic.java

--- Quadratic.java
+++ Quadratic.java
@@ -1,12 +1,4 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
 // WAP to check the root is real ,imaginary,or eqaual;
-
 import java.util.Scanner;
 public class Quadratic
 {
@@ -19,7 +11,6 @@ public class Quadratic
 		int b=num.nextInt();
 		System.out.println("Enter the constant C:");
 		int c=num.nextInt();
-		
 		float D=b*b - 4*a*c;
 		System.out.println("The value of D:"+D);
 		if(D>0){
@@ -28,6 +19,5 @@ public class Quadratic
 		    System.out.println("The root is real and equal");
 		}else
 		System.out.println("The root is imaginary");
-		
-	}
+		}
 }
